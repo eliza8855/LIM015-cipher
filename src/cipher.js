3de791d0c73c8f5f21a63d5ffe6d21137ad8a354
eliza8855 <elopezvalerin@gmail.com>
2021-06-09 00:35:50 -0500

    Terminada sección de javascript

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,58 @@
 const cipher = {
-  // ...
+encode(offset,string){
+  let newWord = "";
+  for (let i=0; i< string.length; i++) {
+  let n = (string.charCodeAt(i) - 65 + offset) % 26 + 65;
+  let str2 = String.fromCharCode(n);
+  newWord += str2
+  };
+  document.getElementById("boxEncode2").innerHTML = newWord;
+},
+decode(offset,string){
+  let newWord = "";
+  for (let i=0; i< string.length; i++) {
+  let n = (string.charCodeAt(i) - 39 - (offset%26))%26 + 65;
+  let str2 = String.fromCharCode(n);
+  newWord += str2
+  };
+  let changeNewWord = newWord.replace(/:/g," ")
+  document.getElementById("boxDecode2").innerHTML = changeNewWord;
+}
 };
-
 export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+/*offset,string*/
+
+
+/*let string = document.getElementById("boxEncode1").value;  // Llamado a caja 1
+  let result = "";
+  let offset = document.getElementById("textOffsetEncode").value; // Llamado al offset
+  for (let i=0; i<string.length; i++) {
+   let n = (string.charCodeAt(i)- 65 + offset) % 26 + 65; // Pasar de letras a ACSII con el offset
+   let stringConvert = String.fromCharCode(n); // Pasar de ACSII a letras normales
+   result += stringConvert; // uniendo las letras en el resultado
+   document.getElementById("boxEncode2").innerHTML = result; // ingresando el resultado a la caja 2
+   }*/
+/*},
+decode(offset,string){
+  /*let string = document.getElementById("boxEncode1").value; 
+  let result = "";
+  let offset = document.getElementById("textOffsetEncode").value;
+  for (let i=0; i<string.length; i++) {
+   let n = (string.charCodeAt(i)- 65 + offset) % 26 + 65;
+   let stringConvert = String.fromCharCode(n);
+   result += stringConvert;
+   document.getElementById("boxEncode2").innerHTML = result;
+   }
+}*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+const capturarInputEncode = () => {
+let offset = parseInt(document.getElementById("textOffsetEncode").value);
+let str = document.getElementById("boxEncode1").value;
+cipher.encode(offset,str)
+};
 
-console.log(cipher);
+document.getElementById("btnOffsetEncode").addEventListener("click", ()=> capturarInputEncode());
+
+const capturarInputDecode = () => {
+    let offset = parseInt(document.getElementById("textOffsetDecode").value);
+    let str = document.getElementById("boxDecode1").value;
+    cipher.decode(offset,str)
+    };
+    
+    document.getElementById("btnOffsetDecode").addEventListener("click", ()=> capturarInputDecode());
+
+
